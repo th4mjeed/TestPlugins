cfba42daa9d67aa677c4d8bee95bcb5894249378
Thamjeed <7h4mj33d@gmail.com>
2026-06-14 20:50:46 +0530

    Remove apiVersion from build.gradle.kts

--- TmdbMovies/build.gradle.kts
+++ TmdbMovies/build.gradle.kts
@@ -7,5 +7,4 @@ cloudstream {
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.themoviedb.org&sz=%size%"
     language = "en"
-    apiVersion = 1
 }
