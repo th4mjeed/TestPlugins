f459d2cfd46fe71b1c673f14406296db04d9b1b7
Thamjeed <7h4mj33d@gmail.com>
2026-06-14 20:28:31 +0530

    Remove isCrossPlatform from build.gradle.kts
    Removed the isCrossPlatform property from the configuration.

--- TmdbMovies/build.gradle.kts
+++ TmdbMovies/build.gradle.kts
@@ -6,6 +6,5 @@ cloudstream {
     status = 1
     tvTypes = listOf("Movie", "TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.themoviedb.org&sz=%size%"
-    isCrossPlatform = true
     language = "en"
 }
