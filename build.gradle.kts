028b271959d353235ca3681dc8d2b937bcfff897
Thamjeed <7h4mj33d@gmail.com>
2026-06-14 19:30:02 +0530

    Change Cloudstream gradle plugin to specific commit
    Updated Cloudstream gradle plugin version to a specific commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:81b1d424d2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
